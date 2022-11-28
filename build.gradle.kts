5d6c1440830b862c256778d84304f04609b032d6
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2022-11-28 16:53:40 -0600

    Migrate to Gradle Kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    application
+    java
+    id("io.ktor.plugin") version "2.1.1" // It builds fat JARs
+    id("io.freefair.lombok") version "6.6-rc1"
+}
+
+group = "io.github.gaming32"
+version = "1.0-SNAPSHOT"
+
+java.sourceCompatibility = JavaVersion.VERSION_11
+java.targetCompatibility = JavaVersion.VERSION_11
+
+application {
+    mainClass.set("io.github.gaming32.superpack.Main")
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = application.mainClass.get()
+        attributes["Multi-Release"] = true
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    implementation("com.github.gaming32:mrpacklib:9d979177f9")
+    implementation("com.formdev:flatlaf:2.6")
+    implementation("com.github.Dansoftowner:jSystemThemeDetector:3.8")
+    implementation("net.java.dev.jna:jna:5.12.1")
+    implementation("net.java.dev.jna:jna-platform:5.12.1")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.19.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.19.0")
+    implementation("com.github.gaming32:pipeline:5984f99cbe")
+    implementation("org.jetbrains:annotations:23.0.0")
+    compileOnly("org.projectlombok:lombok:1.18.24")
+}
+
+tasks.withType<JavaCompile>() {
+    options.encoding = "UTF-8"
+}
--- src/main/java/io/github/gaming32/superpack/tabs/SettingsTab.java
+++ src/main/java/io/github/gaming32/superpack/tabs/SettingsTab.java
@@ -1,30 +1,23 @@
 package io.github.gaming32.superpack.tabs;
 
-import java.awt.Desktop;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-
-import javax.swing.BorderFactory;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-
+import io.github.gaming32.superpack.Superpack;
+import io.github.gaming32.superpack.SuperpackMainFrame;
+import io.github.gaming32.superpack.SuperpackSettings;
+import io.github.gaming32.superpack.themes.Theme;
+import io.github.gaming32.superpack.themes.Themes;
+import io.github.gaming32.superpack.util.GeneralUtil;
+import io.github.gaming32.superpack.util.HasLogger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.github.gaming32.superpack.Superpack;
-import io.github.gaming32.superpack.SuperpackMainFrame;
-import io.github.gaming32.superpack.util.GeneralUtil;
-import io.github.gaming32.superpack.util.HasLogger;
+import javax.swing.*;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.*;
 
 public final class SettingsTab extends JPanel implements HasLogger, SelectedTabHandler {
     public static final Logger LOGGER = LoggerFactory.getLogger(SettingsTab.class);
 
-    @SuppressWarnings("unused")
+    @SuppressWarnings({"unused", "FieldCanBeLocal"})
     private final SuperpackMainFrame parent;
 
     private final JLabel cacheSize;
@@ -36,25 +29,50 @@ public final class SettingsTab extends JPanel implements HasLogger, SelectedTabH
         GridBagConstraints gbc;
         setLayout(new GridBagLayout());
 
-        // {
-        //     final JPanel generalSettings = new JPanel();
+        {
+            final JPanel generalSettings = new JPanel();
 
-        //     final GroupLayout layout = new GroupLayout(generalSettings);
-        //     generalSettings.setLayout(layout);
-        //     layout.setAutoCreateGaps(true);
-        //     layout.setAutoCreateContainerGaps(true);
-        //     layout.setHorizontalGroup(layout.createParallelGroup()
-        //     );
-        //     layout.setVerticalGroup(layout.createSequentialGroup()
-        //     );
-        //     generalSettings.setBorder(BorderFactory.createTitledBorder("General settings"));
+            final JLabel themeWarningLabel = new JLabel("Warning: Theme changes may cause visual issues until a restart");
+            themeWarningLabel.setForeground(Color.RED);
+            themeWarningLabel.setVisible(false);
 
-        //     gbc = new GridBagConstraints();
-        //     gbc.gridx = 0;
-        //     gbc.gridy = 0;
-        //     gbc.insets = new Insets(3, 3, 3, 3);
-        //     add(generalSettings, gbc);
-        // }
+            final JLabel themeLabel = new JLabel("Theme:");
+
+            final JComboBox<Theme> theme = new JComboBox<>(Themes.getThemes());
+            theme.setSelectedItem(SuperpackSettings.INSTANCE.getTheme());
+            theme.addActionListener(ev -> {
+                themeWarningLabel.setVisible(true);
+                //noinspection ConstantConditions
+                Superpack.setTheme((Theme)theme.getSelectedItem());
+                SwingUtilities.updateComponentTreeUI(parent);
+            });
+
+            final GroupLayout layout = new GroupLayout(generalSettings);
+            generalSettings.setLayout(layout);
+            layout.setAutoCreateGaps(true);
+            layout.setAutoCreateContainerGaps(true);
+            layout.setHorizontalGroup(layout.createParallelGroup(Alignment.CENTER)
+                .addGroup(layout.createSequentialGroup()
+                    .addComponent(themeLabel)
+                    .addComponent(theme)
+                )
+                .addComponent(themeWarningLabel)
+            );
+            layout.setVerticalGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(Alignment.CENTER)
+                    .addComponent(themeLabel)
+                    .addComponent(theme)
+                )
+                .addComponent(themeWarningLabel)
+            );
+            generalSettings.setBorder(BorderFactory.createTitledBorder("General settings"));
+
+            gbc = new GridBagConstraints();
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            gbc.insets = new Insets(3, 3, 3, 3);
+            add(generalSettings, gbc);
+        }
 
         {
             final JPanel cacheSettings = new JPanel();
@@ -76,6 +94,7 @@ public final class SettingsTab extends JPanel implements HasLogger, SelectedTabH
                 cacheManageThread = new Thread(() -> {
                     try {
                         GeneralUtil.rmdir(Superpack.CACHE_DIR.toPath());
+                        //noinspection ResultOfMethodCallIgnored
                         Superpack.CACHE_DIR.mkdirs();
                         calculateCacheSize();
                     } catch (Exception ioe) {
