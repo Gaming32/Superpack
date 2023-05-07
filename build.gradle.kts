37ee4ff5572706d892cb7ba2e44584a9c0f4c21e
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2023-05-07 02:11:35 -0500

    Update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,21 +33,22 @@ repositories {
 dependencies {
     implementation("com.github.gaming32:mrpacklib:9d979177f9")
 
-    implementation("com.formdev:flatlaf:3.0")
+    implementation("com.formdev:flatlaf:3.1.1")
 
     implementation("com.github.Dansoftowner:jSystemThemeDetector:3.8")
 
-    implementation("net.java.dev.jna:jna:5.12.1")
-    implementation("net.java.dev.jna:jna-platform:5.12.1")
+    implementation("net.java.dev.jna:jna:5.13.0")
+    implementation("net.java.dev.jna:jna-platform:5.13.0")
 
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.19.0")
-    implementation("org.apache.logging.log4j:log4j-core:2.19.0")
+    implementation("org.slf4j:slf4j-api:2.0.7")
+    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.20.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.20.0")
     implementation("io.github.oshai:kotlin-logging-jvm:4.0.0-beta-22")
 
     implementation("com.github.TheRandomLabs:CurseApi:cdc5d48d5e")
     implementation("org.jsoup:jsoup:1.16.1") // Upgrade from old jsoup version that has CVEs
 
-    compileOnly("org.jetbrains:annotations:24.0.0")
+    compileOnly("org.jetbrains:annotations:24.0.1")
 
     compileOnly("org.projectlombok:lombok:1.18.26")
 }
